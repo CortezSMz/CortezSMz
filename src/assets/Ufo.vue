71c1f4af287163376a65f1f50c45fc37996e2ee3
CortezSMz <cortez_alexandre@hotmail.com>
2022-03-27 21:03:53 -0300

    added some Ufo animations to make it more alive

---
+++ src/assets/Ufo.vue
@@ -0,0 +1,108 @@
+<template>
+  <!-- <?xml version="1.0" encoding="utf-8"?> -->
+  <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <svg
+    ref="ufo"
+    version="1.1"
+    id="ufo"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="-255 297 100 200"
+    style="enable-background: new -255 297 100 200"
+    xml:space="preserve"
+  >
+    <g id="ship">
+      <g id="beam">
+        <path id="light" d="M-204.1,314.6l-50,179.1h100L-204.1,314.6z" />
+
+        <line id="suckLine" x1="-205" y1="489" x2="-205" y2="312.1" />
+      </g>
+      <g id="fuselage">
+        <g id="feets"></g>
+        <path
+          id="inner"
+          d="M-209.9,318.3c3.3-2,8.6-2,11.8,0c3.3,2,3.3,5.3,0,7.3s-8.6,2-11.8,0
+			C-213.3,323.5-213.3,320.3-209.9,318.3z"
+        />
+        <path
+          id="outer"
+          d="M-211.4,314.8c4-2.4,10.5-2.4,14.6,0c4,2.4,4,6.3,0,8.6c-4,2.4-10.5,2.4-14.6,0
+			C-215.4,320.9-215.4,317-211.4,314.8z"
+        />
+        <path
+          id="pit"
+          d="M-204.1,309.6c3.6,0,6.4,2.9,6.4,6.4s-2.9,5.2-6.4,5.2s-6.4-1.6-6.4-5.2S-207.7,309.6-204.1,309.6z"
+        />
+      </g>
+    </g>
+    <g id="et">
+      <path
+        id="body"
+        d="M-200.4,320.5c0.1-0.4,0.2-0.8,0.2-1.2v-3.2c0-2.1-1.7-3.9-3.9-3.9c-2.1,0-3.9,1.7-3.9,3.9v3.2
+		c0,0.4,0.1,0.8,0.2,1.2c1,0.5,2.3,0.7,3.7,0.7C-202.7,321.3-201.4,321-200.4,320.5z"
+      />
+      <g id="eye">
+        <path
+          id="iris"
+          d="M-204.1,314.9c1.1,0,1.9,0.9,1.9,1.9c0,1.1-0.9,1.9-1.9,1.9c-1.1,0-1.9-0.9-1.9-1.9
+			S-205.2,314.9-204.1,314.9z"
+        />
+        <path
+          id="pupil"
+          d="M-204.1,316.1c0.4,0,0.6,0.3,0.6,0.6c0,0.4-0.3,0.6-0.6,0.6s-0.6-0.3-0.6-0.6
+			S-204.4,316.1-204.1,316.1z"
+        />
+      </g>
+    </g>
+    <linearGradient
+      id="beam_1_"
+      gradientUnits="userSpaceOnUse"
+      x1="-1011.8405"
+      y1="1.6647"
+      x2="-1011.8405"
+      y2="2.6647"
+      gradientTransform="matrix(100 0 0 179.1 100980 16.5014)"
+    >
+      <stop offset="0" style="stop-color: #00ffff; stop-opacity: 0.75" />
+      <stop offset="1" style="stop-color: #00ffff; stop-opacity: 0" />
+    </linearGradient>
+  </svg>
+</template>
+
+<style scoped>
+#light {
+  fill: url(#beam_1_);
+}
+#pupil,
+#outer,
+#feets {
+  fill: #324d5b;
+}
+#inner {
+  fill: #f4b459;
+}
+#pit {
+  fill: #e4e7e7;
+}
+#et {
+  fill: #3db39e;
+}
+#iris {
+  fill: #ffffff;
+}
+</style>
+
+<script lang="ts">
+import { animateUfo } from "@/plugins/animejs/Ufo";
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Ufo",
+
+  mounted() {
+    animateUfo(this.$refs.ufo as Element);
+  },
+});
+</script>
--- src/plugins/animejs/Ufo.ts
+++ src/plugins/animejs/Ufo.ts
@@ -1,14 +1,117 @@
 import anime from "animejs";
 
-function suckText(element: Element): void {
-  element.innerHTML = element.textContent
-    ? element.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+function animateUfo(ufo: Element): void {
+  const [ship, et] = ufo.childNodes;
+
+  /* Et animations */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_body, eye] = et.childNodes;
+  const [iris, pupil] = eye.childNodes;
+
+  /* eye blink */
+  anime
+    .timeline({ loop: true, direction: "alternate" })
+    .add({
+      targets: iris,
+      baseFrequency: 0,
+      scale: 1,
+      easing: "easeInOutQuint",
+      d: [
+        {
+          value:
+            "M-204.1,314.9c1.1,0,1.9,0.9,1.9,1.9c0,1.1-0.9,1.9-1.9,1.9c-1.1,0-1.9-0.9-1.9-1.9 S-205.2,314.9-204.1,314.9z",
+        },
+        {
+          value:
+            "M-204.1,316.3c1.1,0,1.9,0.2,1.9,0.4c0,0.3-0.9,0.4-1.9,0.4c-1.1,0-1.9-0.2-1.9-0.4 S-205.2,316.3-204.1,316.3z",
+        },
+      ],
+    })
+    .add(
+      {
+        targets: pupil,
+        baseFrequency: 0,
+        easing: "easeInOutQuint",
+        d: [
+          {
+            value:
+              "M-204.1,316.1c0.4,0,0.6,0.3,0.6,0.6c0,0.4-0.3,0.6-0.6,0.6s-0.6-0.3-0.6-0.6 S-204.4,316.1-204.1,316.1z",
+          },
+          {
+            value:
+              "M-204.1,316.6c0.4,0,0.6,0.1,0.6,0.1c0,0.1-0.3,0.1-0.6,0.1s-0.6-0.1-0.6-0.1 S-204.4,316.6-204.1,316.6z",
+          },
+        ],
+      },
+      0
+    );
+
+  /* eye movement */
+  (function moveEye() {
+    const x = anime.random(-0.3, 0.3),
+      y = anime.random(-0.3, 0.3),
+      delay = anime.random(1, 0);
+    anime({
+      targets: pupil,
+      translateX: x * 2,
+      translateY: y * 2,
+      delay,
+    });
+    anime({
+      targets: iris,
+      translateX: x,
+      translateY: y,
+      delay,
+      complete: moveEye,
+    });
+  })();
+
+  /* Ship animations */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_beam, fuselage] = ship.childNodes;
+  const [feets, inner] = fuselage.childNodes;
+
+  /* Feets moving */
+  const path = anime.path(inner as SVGElement);
+
+  const amountOfFeets = 10;
+  const duration = 2500;
+
+  for (let i = 0; i < amountOfFeets; i++) {
+    const feet = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    feet.setAttributeNS(null, "cx", "-255");
+    feet.setAttributeNS(null, "cy", "297");
+    feet.setAttributeNS(null, "r", "1");
+    feet.setAttributeNS(null, "id", "feets");
+    feets.appendChild(feet);
+
+    setTimeout(() => {
+      anime({
+        loop: true,
+        duration: duration,
+        targets: feet,
+        translateX: path("x"),
+        translateY: path("y"),
+        easing: "linear",
+        endDelay: -1,
+        delay: 0,
+      });
+    }, (duration / amountOfFeets) * i);
+  }
+}
+
+function suckText(text: Element): void {
+  text.innerHTML = text.textContent
+    ? text.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
     : "";
 
   anime
     .timeline({ loop: false })
     .add({
-      targets: [element, Array.from(element.children)],
+      targets: [text, Array.from(text.children)],
       translateY: [0, -500],
       opacity: [1, 0],
       easing: "easeInExpo",
@@ -16,27 +119,10 @@ function suckText(element: Element): void {
       delay: (el, i) => 100 + 30 * i * -1,
     })
     .add({
-      targets: [element, Array.from(element.children)],
+      targets: [text, Array.from(text.children)],
       opacity: [0, 1],
       translateY: [0, 0],
     });
 }
 
-function suckImage(element: Element): void {
-  anime
-    .timeline({ loop: false })
-    .add({
-      targets: element,
-      translateY: [0, -10500],
-      opacity: [1, 1],
-      easing: "easeInExpo",
-      duration: 1200,
-    })
-    .add({
-      targets: element,
-      opacity: [0, 1],
-      translateY: [0, 0],
-    });
-}
-
-export { suckText, suckImage };
+export { animateUfo, suckText };
