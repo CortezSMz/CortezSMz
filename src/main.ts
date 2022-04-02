d76c0e0deb8398d5e3c4e69b9f6da6573bfc3fb1
CortezSMz <cortez_alexandre@hotmail.com>
2022-04-01 22:05:44 -0300

    remove unused deps and change default favicon

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free";
-import "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/free-regular-svg-icons";
-import "@fortawesome/free-solid-svg-icons";
 
 Vue.config.productionTip = false;
 
